fe0dd7ef02a68a08d6398d31630784ad7db3ac5d
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-08 20:10:47 -0600

    feat(users): 🔨 Export the findAll, findById and findByQuery handler functions from users directory

---
+++ src/functions/users/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Este archivo index es solamente para exportar todas las funciones
+ * directamente desde la carpeta raiz 'users'
+ */
+export { findAll } from './findAll';
+export { findById } from './findById';
+export { findByQuery } from './findByQuery';
