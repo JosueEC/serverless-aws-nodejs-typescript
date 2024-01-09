d3429d8ff3d46d9dc51f4f1e5a1e4a24df3a21e5
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-08 20:09:58 -0600

    feat(functions): 🔨 Export all the users functions

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,5 @@
-export { default as hello } from './hello';
+/**
+ * Y este index es para exportar todas las funciones en general, ya
+ * no solo por entidad como en el index dentro de cada entidad.
+ */
+export * from './users';
