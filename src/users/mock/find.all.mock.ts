953bbd96d7079a182e423e2b32555f41615c1490
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-11 22:44:39 -0600

    feat(users/mock): 🔨 Add the find.all.mock.ts

---
+++ src/users/mock/find.all.mock.ts
@@ -0,0 +1,17 @@
+export const findAllMock = [
+    {
+        id: 1,
+        fullname: 'Josue',
+        age: 28,
+    },
+    {
+        id: 2,
+        fullname: 'Elena',
+        age: 33,
+    },
+    {
+        id: 3,
+        fullname: 'Marcos',
+        age: 43,
+    },
+];
